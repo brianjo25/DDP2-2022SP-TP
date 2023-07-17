f85673808225302cdcd5401981b606d86dfb9c4d
unknown <bejon.loekito@gmail.com>
2023-07-17 18:02:48 +0700

    Menyelesaikan file HaloDDP

--- TP3/HaloDDP.java
+++ TP3/HaloDDP.java
@@ -46,19 +46,85 @@ public class HaloDDP {
 
             if (menu.equals("1")) {
                 // TODO : Implementasi input obat
+                System.out.print("Masukkan nama obat: ");
+                String namaObat = input.nextLine();
+
+                System.out.print("Masukkan kategori obat: ");
+                String kategoriObat = input.nextLine();
+                // Cek kategori valid
+                Boolean kategoriValid = false;
+                for (int i = 0; i < rowLemari ; i++ ) {
+                    Rak rak = lemari.getRak(i);
+                    if (rak.getKategoriRak().equals(kategoriObat)) {
+                        kategoriValid = true;
+                        System.out.println("Kategori obat valid");
+                    }
+                }
+                if (!kategoriValid) {
+                    System.out.println("Kategori obat tidak valid");
+                    continue;
+                }
+                System.out.print("Masukkan posisi obat: ");
+                String posisiObat = input.nextLine();
+                // Cek kategori obat valid
+                while (!posisiObat.matches("\\d+,\\d+")) {
+                    System.out.println("Format posisi obat salah");
+                    System.out.print("Masukkan posisi obat: ");
+                    posisiObat = input.nextLine();
+                }
+
+                String[] posisiObatSplit = posisiObat.split(",");
+                int row = Integer.valueOf(posisiObatSplit[0]);
+                int column = Integer.valueOf(posisiObatSplit[1]);
+                Rak rak = null;
+                if (rowLemari >= row) {
+                    rak = lemari.getRak(row-1);
+                    if (rak.getKategoriRak().equals(kategoriObat)) {
+                        // System.out.println("Kategori rak valid");
+                    }
+                    else {
+                        System.out.println("Kategori rak tidak valid");
+                        continue;
+                    }
+                }
+                else {
+                    System.out.println("Posisi tidak ada di lemari");
+                }
+
+                System.out.print("Masukkan stok obat: ");
+                int stokObat = Integer.parseInt(input.nextLine());
+                
+                //TODO: Hitung harga
+
+                Obat obat = new Obat(namaObat, stokObat, kategoriObat);
+                obat.setHarga(5000 + (row-1)*10000 + (column-1)*5000);
+                rak.tambahObat(obat, column-1);
             } else if (menu.equals("2")) {
                 // TODO : Implementasi print obat
+                    lemari.print();
             } else if (menu.equals("3")) {
                 // TODO : Implementasi beli obat
-            } else if (menu.equals("99")){
-                // TODO : Implementasi keluar
-                break;
-            } else {
-                System.out.println("Menu tidak tersedia");
+                System.out.print("Masukkan nama obat yang ingin dibeli: ");
+                String namaObat = input.nextLine();
+                System.out.print("Masukkan jumlah obat yang ingin dibeli: ");
+                int jumlahObat = Integer.parseInt(input.nextLine());
+
+                if (lemari.beliObat(namaObat, jumlahObat)) {
+                    System.out.println("Berhasil membeli obat");
+                } else {
+                    System.out.println("Gagal membeli obat");
+                } else if (menu.equals("99")){
+                    // TODO : Implementasi keluar
+                    //TODO: Riwayat Transaksi
+                    break;
+                } else {
+                    System.out.println("Menu tidak tersedia");
+                }
             }
-        }
 
         input.close();
         System.out.println("Terima kasih telah menggunakan Haloddp!");
     }
 }
+}
+
