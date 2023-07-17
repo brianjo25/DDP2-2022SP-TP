2729cdd79aff44d263aacf96e4bbccc96c259890
unknown <bejon.loekito@gmail.com>
2023-07-18 01:55:45 +0700

    Memasukan java doc

--- TP3/Rak.java
+++ TP3/Rak.java
@@ -1,7 +1,17 @@
+/**
+ * Representasi rak obat dalam sistem HaloDDP
+ */
+
 public class Rak {
     private int size;
     private String kategori;
     private Obat[] daftarObat;
+
+    /**
+     * Konstruktor untuk membuat objek rak obat dengan ukuran dan kategori yang ditentukan
+     * @param size untuk ukuran rak obat
+     * @param kategori untuk kategori rak obat
+     */
 
     public Rak(int size, String kategori){
         this.kategori = kategori;
@@ -9,16 +19,31 @@ public class Rak {
         this.size = size;
     }
 
+    /**
+     * Mengembalikan kategori dari rak
+     * @return kategori rak obat
+     */
+
     // TODO : Implementasi method getKategoriRak
     public String getKategoriRak() {
         return kategori;
     }
+
+    /**
+     * Menambahkan obat ke dalam rak obat pada indeks yang ditentukan
+     * @param obat untuk obat yang akan ditambahkan
+     * @param index untuk indeks tempat penambahan obat
+     */
 
     // TODO : Implementasi method tambahObat
     public void tambahObat(Obat obat, int index) {
         this.daftarObat[index] = obat;
     }
     
+    /**
+     * Print isi rak obat ke layar
+     */
+
     // TODO : Implementasi method printRak
     public void printRak(){
             for (int j = 0 ; j < this.size ; j++){
@@ -32,6 +57,10 @@ public class Rak {
             System.out.println();
         }
     
+    /**
+     * Mengembalikan daftar obat yang ada dalam rak obat
+     * @return untuk array obat yang ada dalam rak obat
+     */
 
     // TODO : Implementasi method getListObat
     public Obat[] getListObat() {
